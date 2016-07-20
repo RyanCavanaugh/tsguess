766922df0a97b90c703fe0563072e29c72ca5187
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2016-07-20 15:27:55 -0700

    Refactor

--- src/run.ts
+++ src/run.ts
@@ -2,21 +2,26 @@ import * as guess from './index';
 import * as yargs from 'yargs';
 
 interface Options {
+	identifier?: string;
 	expression?: string;
 	module?: string;
 }
 const args: Options = yargs
-	.alias('e', 'expression')
+	//.alias('i', 'identifier')
+	//.alias('e', 'expression')
 	.alias('m', 'module')
 	.argv;
 
 
 const opts: Options = yargs.argv;
 
-let expr: any = undefined;
-if (args.expression) {
-	expr = eval(args.expression);
-} else if (args.module) {
-	expr = require(args.module);
+if (args.module) {
+	console.log(guess.generateModuleDeclarationFile(args.module));
+} else if(args.expression) {
+	console.log(guess.generateExpressionDeclarationFile(args.expression));
+} else if(args.identifier) {
+	console.log(guess.generateExpressionDeclarationFile(args.identifier));
+} else {
+	console.log('Usage: tsguess -m moduleName');
+	console.log('Example: tsguess -m fs');
 }
-console.log(guess.guess(expr));
